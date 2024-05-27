1696028a07522a71a41b5fafda1265def0cc8b63
leona <leonardovieiraxy@hotmail.com>
2024-05-27 15:17:23 -0300

    Changes to be committed: 	renamed:    SpringBootIntroducao/Readme.md -> SpringBootIntroducaoConceitosAPI/Readme.md 	renamed:    SpringBootIntroducao/first/.gitignore -> SpringBootIntroducaoConceitosAPI/first/.gitignore 	renamed:    SpringBootIntroducao/first/.mvn/wrapper/maven-wrapper.jar -> SpringBootIntroducaoConceitosAPI/first/.mvn/wrapper/maven-wrapper.jar 	renamed:    SpringBootIntroducao/first/.mvn/wrapper/maven-wrapper.properties -> SpringBootIntroducaoConceitosAPI/first/.mvn/wrapper/maven-wrapper.properties 	renamed:    SpringBootIntroducao/first/mvnw -> SpringBootIntroducaoConceitosAPI/first/mvnw 	renamed:    SpringBootIntroducao/first/mvnw.cmd -> SpringBootIntroducaoConceitosAPI/first/mvnw.cmd 	renamed:    SpringBootIntroducao/first/pom.xml -> SpringBootIntroducaoConceitosAPI/first/pom.xml 	renamed:    SpringBootIntroducao/first/src/main/java/com/project/first/FirstApplication.java -> SpringBootIntroducaoConceitosAPI/first/src/main/java/com/project/first/FirstApplication.java 	renamed:    SpringBootIntroducao/first/src/main/java/com/project/first/controllers/HelloWorldController.java -> SpringBootIntroducaoConceitosAPI/first/src/main/java/com/project/first/controllers/HelloWorldController.java 	renamed:    SpringBootIntroducao/first/src/main/resources/application.properties -> SpringBootIntroducaoConceitosAPI/first/src/main/resources/application.properties 	renamed:    SpringBootIntroducao/first/src/test/java/com/project/first/FirstApplicationTests.java -> SpringBootIntroducaoConceitosAPI/first/src/test/java/com/project/first/FirstApplicationTests.java 	renamed:    SpringBootIntroducao/produto/.gitignore -> SpringBootIntroducaoConceitosAPI/produto/.gitignore 	renamed:    SpringBootIntroducao/produto/.mvn/wrapper/maven-wrapper.jar -> SpringBootIntroducaoConceitosAPI/produto/.mvn/wrapper/maven-wrapper.jar 	renamed:    SpringBootIntroducao/produto/.mvn/wrapper/maven-wrapper.properties -> SpringBootIntroducaoConceitosAPI/produto/.mvn/wrapper/maven-wrapper.properties 	renamed:    SpringBootIntroducao/produto/date.sql -> SpringBootIntroducaoConceitosAPI/produto/date.sql 	renamed:    SpringBootIntroducao/produto/mvnw -> SpringBootIntroducaoConceitosAPI/produto/mvnw 	renamed:    SpringBootIntroducao/produto/mvnw.cmd -> SpringBootIntroducaoConceitosAPI/produto/mvnw.cmd 	renamed:    SpringBootIntroducao/produto/pom.xml -> SpringBootIntroducaoConceitosAPI/produto/pom.xml 	renamed:    SpringBootIntroducao/produto/src/main/java/com/cadastro/produto/ProdutoApplication.java -> SpringBootIntroducaoConceitosAPI/produto/src/main/java/com/cadastro/produto/ProdutoApplication.java 	renamed:    SpringBootIntroducao/produto/src/main/java/com/cadastro/produto/controllers/ProdutoController.java -> SpringBootIntroducaoConceitosAPI/produto/src/main/java/com/cadastro/produto/controllers/ProdutoController.java 	renamed:    SpringBootIntroducao/produto/src/main/java/com/cadastro/produto/entities/Produto.java -> SpringBootIntroducaoConceitosAPI/produto/src/main/java/com/cadastro/produto/entities/Produto.java 	renamed:    SpringBootIntroducao/produto/src/main/java/com/cadastro/produto/repositories/ProdutoRepository.java -> SpringBootIntroducaoConceitosAPI/produto/src/main/java/com/cadastro/produto/repositories/ProdutoRepository.java 	renamed:    SpringBootIntroducao/produto/src/main/resources/application.properties -> SpringBootIntroducaoConceitosAPI/produto/src/main/resources/application.properties 	renamed:    SpringBootIntroducao/produto/src/test/java/com/cadastro/produto/ProdutoApplicationTests.java -> SpringBootIntroducaoConceitosAPI/produto/src/test/java/com/cadastro/produto/ProdutoApplicationTests.java 	renamed:    SpringBoot/Readme.md -> SpringBootParametrosEndpoints/Readme.md

---
+++ SpringBootIntroducaoConceitosAPI/first/src/test/java/com/project/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootIntroducaoConceitosAPI.first.src.test.java.com.project.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
