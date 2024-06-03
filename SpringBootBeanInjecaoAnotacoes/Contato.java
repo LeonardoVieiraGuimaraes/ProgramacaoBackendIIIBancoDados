8b607024868f6ceb39ed08ebc8c793adfb1b37cb
leona <leonardovieiraxy@hotmail.com>
2024-06-03 10:22:08 -0300

    Changes to be committed: 	new file:   SpringBootBeanInjecaoAnotacoes/Contato.java 	new file:   SpringBootBeanInjecaoAnotacoes/MeuBean.java

---
+++ SpringBootBeanInjecaoAnotacoes/Contato.java
@@ -0,0 +1,37 @@
+package SpringBootBeanInjecaoAnotacoes;
+
+
+public String Contato {
+
+    
+}
+private String cpf, nome, email, celular; 
+
+punic String qetCpf() {
+return cpf;
+enaiL ,
+celular;
+public void setCpf(String cpf)
+this.cpf
+cpf;
+[ / *notoç50. para c campo
+punic strlng {
+return nane ;
+public void setNane(String nome) {
+tras. nome nome;
+// para
+public String getEMài7-()
+return email;
+PUDIfc voi3 seroe 1 (String enait)
+thlg- enalt;
+'F
+pubUc String getCeLut-ar() f
+return celular;
+"nome"
+" n orne"
+ao classe conraro
+da Classe contato
+public vald setCe1a1ar•CStrinq celular)
+this.
+celular;
+'r
