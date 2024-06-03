b77a1c11e162a9f34a4def0d330beba30010b565
leona <leonardovieiraxy@hotmail.com>
2024-06-03 10:28:30 -0300

    Changes to be committed: 	modified:   SpringBootBeanInjecaoAnotacoes/Contato.java

--- SpringBootBeanInjecaoAnotacoes/Contato.java
+++ SpringBootBeanInjecaoAnotacoes/Contato.java
@@ -1,37 +1,43 @@
 package SpringBootBeanInjecaoAnotacoes;
 
 
-public String Contato {
+public class  Contato {
 
-    
+    private String cpf, nome, email, celular; 
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+
 }
-private String cpf, nome, email, celular; 
 
-punic String qetCpf() {
-return cpf;
-enaiL ,
-celular;
-public void setCpf(String cpf)
-this.cpf
-cpf;
-[ / *notoç50. para c campo
-punic strlng {
-return nane ;
-public void setNane(String nome) {
-tras. nome nome;
-// para
-public String getEMài7-()
-return email;
-PUDIfc voi3 seroe 1 (String enait)
-thlg- enalt;
-'F
-pubUc String getCeLut-ar() f
-return celular;
-"nome"
-" n orne"
-ao classe conraro
-da Classe contato
-public vald setCe1a1ar•CStrinq celular)
-this.
-celular;
-'r
+
